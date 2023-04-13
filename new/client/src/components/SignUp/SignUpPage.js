91fafa5695fa18b9fedfe8a059937a4cd06780dd
Deandre Gardener <dgardener02@gmail.com>
2023-04-12 21:08:58 -0500

    2ndcommit

--- new/client/src/components/SignUp/SignUpPage.js
+++ new/client/src/components/SignUp/SignUpPage.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import SignUpForm from './SignUpForm';
+import React from "react";
+import SignUpForm from "./SignUpForm";
+import "./signup.css";
 
 class LoginPage extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="col-md-4 col-md-offset-4">
-          <SignUpForm />
-        </div>
+        {/* <div className="col-md-4 col-md-offset-4"> */}
+        <SignUpForm />
+        {/* </div> */}
       </div>
     );
   }
