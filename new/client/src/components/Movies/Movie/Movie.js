305c0f89d3110c090bb206e26c744b34cf2c2d73
Gabe Granado <gabegranado@Gabes-MacBook-Pro.local>
2023-04-20 18:41:51 -0500

    finished movieDetails bug and added getImage and movieDetails

--- new/client/src/components/Movies/Movie/Movie.js
+++ new/client/src/components/Movies/Movie/Movie.js
@@ -1,45 +1,21 @@
 import React from 'react';
-import girlWithTheDragonTattoo from '../../../images/girlWithTheDragonTattoo.png';
-import parasite from '../../../images/parasite.png';
-import uncutgems from '../../../images/uncutgems.png'
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useNavigate } from "react-router-dom";
 import { browserHistory } from 'react-router';
 import moviedetails from '../../../images/movieDetail.json'
-
-// import { likePost, deletePost } from '../../../actions/posts';
-import useStyles from './style';
+import { getImage } from '../../../images/getImage';
 
 const Movie = ({ movie, setCurrentId }) => {
   const navigate = useNavigate();
-  const options = [
-    ['parasite', parasite],
-    ['girlwiththedragontattoo', girlWithTheDragonTattoo],
-    ['uncutgems', uncutgems]
-  ]
-
-  const getPoster = () => {
-    for (let i in options) {
-      // console.log("HEY LOOK HERE RIGHT HERE", movie.movieTitle);
-      // console.log(typeof movie.movieTitle);
-      // console.log("hey look here ", i, options[i][0]);
-      if (options[i][0] == movie.movieTitle) { return options[i][1] };
-    }
-    return 'test';
-    };
 
   const redirctToMoiveDetails = () => {
-    console.log("herhe", movie);
-    console.log("movieID: ", movie._id)
     browserHistory.push('Home');
     navigate(`/movieDetails/${movie._id}`);
   }
-  console.log(moviedetails.parasiteSummary)
-// 
+
   return (
     <div>
-      {/* <h1>{getPoster()}</h1> */}
-    <img  src={getPoster()} alt={getPoster()}/>
+    <img  src={getImage(movie.movieTitle)}/>
     <Button variant="contained" color="secondary" size="small" onClick={redirctToMoiveDetails} fullWidth>Movie details</Button>
     </div>
   );
--- new/client/src/components/Movies/MoviesDetails.js
+++ new/client/src/components/Movies/MoviesDetails.js
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import '../Movies/Movie/MovieDetails.css'
-import moviedetails from '../../images/movieDetail.json'
+import allMovieDetails from '../../images/movieDetail.json'
 import BuyMovieForm from '../BuyMovie/BuyMovieForm.js'
 import { render } from "react-dom";
 import { Component } from "react";
 import Popup from "../Popup";
 import { useParams } from "react-router-dom";
 import { getMovie } from "../../actions/movie.js";
+import { getImage } from "../../images/getImage.js";
 
 const MovieDetails = () => {
   const dispatch = useDispatch();
   const params = useParams();
   //`http://localhost:3000/movie/findMovieById/${params.movieId}`
-  const movieTitles = [['parasite', moviedetails.parasiteSummary], ['thegirlwiththedragontatto', moviedetails.thegirlwiththedragontattoSummary], ['uncutgems', moviedetails.uncutgemsSummary]]
   const [buttonPopup, setButtonPopup] = useState(false);
   var movieTitle = '';
+  var movieSummary = '';
 
   useEffect(() => {
     console.log("movieDetails: ", params.movieId);
@@ -27,16 +28,24 @@ const MovieDetails = () => {
   
   if (movie) {
     movieTitle = movie.movieTitle;
+    const parsed = JSON.parse(JSON.stringify(allMovieDetails));
+    var movieDetails = '';
+
+    for (var key in parsed) {
+      if (key == movieTitle) {
+        movieDetails = parsed[key]
+      }
+    }
   }
 
   return (
     <div className="movie-details">
       <main>
-        <h1> test {movieTitle}</h1>
+        <h1> test {movieDetails.title}</h1>
         <br></br>
         <button onClick={() => setButtonPopup(true)}>BuyTickets</button>
       </main>
-
+      <img src={getImage(movieTitle)} />
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
         <BuyMovieForm/>
         </Popup>
