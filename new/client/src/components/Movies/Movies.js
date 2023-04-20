6b1a316ddba791a9d2a587772f7f0efda9a12dd3
Deandre Gardener <dgardener02@gmail.com>
2023-04-19 23:19:10 -0500

    homepage changes

--- new/client/src/components/Movies/Movies.js
+++ new/client/src/components/Movies/Movies.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import Movie from './Movie/Movie';
-import { Grid, CircularProgress } from '@material-ui/core';
-import { useSelector } from 'react-redux';
+import React from "react";
+import Movie from "./Movie/Movie";
+import { Grid, CircularProgress } from "@material-ui/core";
+import { useSelector } from "react-redux";
 
-import useStyles from './style';
-
+import useStyles from "./style";
 
 const Movies = ({ setCurrentId }) => {
-    const movies = useSelector((state) => state.movies);
-    const classes = useStyles();
+  const movies = useSelector((state) => state.movies);
+  // const classes = useStyles();
 
-    console.log("hmmm" + movies);
-    return (
-        !movies.length ? <CircularProgress /> : (
-          <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-            {movies.map((movie) => (
-              <Grid key={movie._id} item xs={12} sm={6} md={6}>
-                <Movie movie={movie} setCurrentId={setCurrentId} />
-              </Grid>
-            ))}
-          </Grid>
-        )
-      );
-    };
+  console.log("hmmm" + movies);
+  return !movies.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid>
+      {movies.map((movie) => (
+        <Grid>
+          <Movie movie={movie} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+  // <div>
+  //   <Movie movie={movie} setCurrentId={setCurrentId} />
+  // </div>
+};
 
 export default Movies;
